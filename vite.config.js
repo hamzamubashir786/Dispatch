2383ab78bcb0ba8a231e75c71e394340f7db6a89
Hamza Mubashir <167816304+hamzamubashir786@users.noreply.github.com>
2025-03-25 19:51:09 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // Ensures correct asset paths
+  plugins: [react()],
+  base: "/", // Make sure it's set correctly
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    assetsDir: "assets", // Ensures assets are in a separate folder
   }
 });
-
